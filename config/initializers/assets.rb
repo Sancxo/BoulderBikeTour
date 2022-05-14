7c5356bead1a099d49f7e00c8555af19b03534b1
Simon <simon@7lieues.io>
2022-05-14 21:54:12 +0200

    build(env_variables): initializes local_env.yml in assets config

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,6 +3,14 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = "1.0"
 
+Rails.application.config.before_configuration do
+    env_file = File.join(Rails.root, 'config', 'local_env.yml')
+    YAML.load(File.open(env_file)).each do |key, value|
+      ENV[key.to_s] = value
+    end if File.exists?(env_file)
+end
+  
+
 # Precompile Bootstrap and Popper.js
 Rails.application.config.assets.precompile += %w( bootstrap.min.js popper.js )
 
