80c9dbcca209fdba69c6ce47d47d733a23637435
Simon <simon@7lieues.io>
2022-05-27 15:23:47 +0200

    refactor(jumbotron_controller): accesses counter container element the Stimulus way

--- app/javascript/controllers/jumbotron_controller.js
+++ app/javascript/controllers/jumbotron_controller.js
@@ -1,12 +1,13 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
+    static targets = ["counterContainer"];
+    
     connect() {
         const navBar = document.getElementById("navbar");
 
         this.element.style.height = `calc(100vh - ${navBar.offsetHeight}px)`;
 
-        const counterContainer = document.getElementById('counter-container');
-        counterContainer.classList.replace("d-none", "d-block");
+        this.counterContainerTarget.classList.replace("d-none", "d-block");
     }
 }
