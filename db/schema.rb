95a981010e48ef0786e9a71b7b4fc5c1e0c8e5d7
Simon <simon@7lieues.io>
2022-06-09 21:42:54 +0200

    feat(contest): creates submission model and schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_03_093546) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_09_185250) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_03_093546) do
     t.datetime "updated_at", null: false
     t.string "gender"
     t.decimal "lat_long", default: [], array: true
+  end
+
+  create_table "submissions", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.text "slogan"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
