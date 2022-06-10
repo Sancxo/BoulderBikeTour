6e3c2cf4887f524ad152ab2baa31d7167f235569
Simon <simon@7lieues.io>
2022-06-10 12:18:00 +0200

    feat: adds validation to Riders and Submissions models

--- app/models/rider.rb
+++ app/models/rider.rb
@@ -1,2 +1,5 @@
 class Rider < ApplicationRecord
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :lat_long, presence: true
 end
--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,2 +1,9 @@
 class Submission < ApplicationRecord
+    EMAIL_FORMAT = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :email, presence: true, format: {with: EMAIL_FORMAT, 
+        message: "Your email address should respect the correct email address format."}
+    validates :slogan, presence: true, length: {in: 2..50}
 end
