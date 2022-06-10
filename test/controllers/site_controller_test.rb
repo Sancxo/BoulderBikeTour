82387543f2a4d9530520f29603f7a4f7a8d62f56
Simon <simon@7lieues.io>
2022-06-10 15:33:12 +0200

    tests: adds email confirmation to tests + tests confirmation

--- test/controllers/site_controller_test.rb
+++ test/controllers/site_controller_test.rb
@@ -32,15 +32,23 @@ class SiteControllerTest < ActionDispatch::IntegrationTest
 
   test "should create submission" do
     assert_difference('Submission.count') do
-      post contest_url, params: {submission: {first_name: 'Test', last_name: 'McTest', email: 'test@test.test', slogan: 'This is my test, for sure!'}}
+      post contest_url, params: {submission: {first_name: 'Test', last_name: 'McTest', email: 'test@test.test', email_confirmation: 'test@test.test', slogan: 'This is my test, for sure!'}}
     end
 
     assert_redirected_to submissions_url
   end
 
-  test "should not create submission because of email format" do
+  test "should create submission even if email confirmation is uppercase" do
+    assert_difference('Submission.count') do
+      post contest_url, params: {submission: {first_name: 'Test', last_name: 'McTest', email: 'test@test.test', email_confirmation: 'TEST@TEST.TEST', slogan: 'This is my test, for sure!'}}
+    end
+
+    assert_redirected_to submissions_url
+  end
+
+  test "should not create submission because of bad email format" do
     assert_no_difference('Submission.count') do
-      post contest_url, params: {submission: {first_name: 'Test', last_name: 'McTest', email: 'test.test', slogan: 'This is my test, for sure!'}}
+      post contest_url, params: {submission: {first_name: 'Test', last_name: 'McTest', email: 'test.test', email_confirmation: 'test.test', slogan: 'This is my test, for sure!'}}
     end
 
     assert_response :unprocessable_entity
@@ -48,7 +56,7 @@ class SiteControllerTest < ActionDispatch::IntegrationTest
 
   test "should not create submission because of missing first_name" do
     assert_no_difference('Submission.count') do
-      post contest_url, params: {submission: {first_name: '', last_name: 'McTest', email: 'test@test.test', slogan: 'This is my test, for sure!'}}
+      post contest_url, params: {submission: {first_name: '', last_name: 'McTest', email: 'test@test.test', email_confirmation: 'test@test.test', slogan: 'This is my test, for sure!'}}
     end
 
     assert_response :unprocessable_entity
@@ -56,7 +64,7 @@ class SiteControllerTest < ActionDispatch::IntegrationTest
 
   test "should not create submission because of missing last_name" do
     assert_no_difference('Submission.count') do
-      post contest_url, params: {submission: {first_name: 'Test', last_name: '', email: 'test@test.test', slogan: 'This is my test, for sure!'}}
+      post contest_url, params: {submission: {first_name: 'Test', last_name: '', email: 'test@test.test', email_confirmation: 'test@test.test', slogan: 'This is my test, for sure!'}}
     end
 
     assert_response :unprocessable_entity
@@ -64,7 +72,7 @@ class SiteControllerTest < ActionDispatch::IntegrationTest
 
   test "should not create submission because of missing slogan" do
     assert_no_difference('Submission.count') do
-      post contest_url, params: {submission: {first_name: 'Test', last_name: 'McTest', email: 'test@test.test', slogan: ''}}
+      post contest_url, params: {submission: {first_name: 'Test', last_name: 'McTest', email: 'test@test.test', email_confirmation: 'test@test.test', slogan: ''}}
     end
 
     assert_response :unprocessable_entity
@@ -72,7 +80,7 @@ class SiteControllerTest < ActionDispatch::IntegrationTest
 
   test "should not create submission because of too short slogan" do
     assert_no_difference('Submission.count') do
-      post contest_url, params: {submission: {first_name: 'Test', last_name: 'McTest', email: 'test@test.test', slogan: 'Y'}}
+      post contest_url, params: {submission: {first_name: 'Test', last_name: 'McTest', email: 'test@test.test', email_confirmation: 'test@test.test', slogan: 'Y'}}
     end
 
     assert_response :unprocessable_entity
@@ -80,9 +88,15 @@ class SiteControllerTest < ActionDispatch::IntegrationTest
 
   test "should not create submission because of too long slogan" do
     assert_no_difference('Submission.count') do
-      post contest_url, params: {submission: {first_name: 'Test', last_name: 'McTest', email: 'test@test.test', slogan: 'Lorem ipsum dolor sit amet, consectetur adipiscing sed.'}}
+      post contest_url, params: {submission: {first_name: 'Test', last_name: 'McTest', email: 'test@test.test', email_confirmation: 'test@test.test', slogan: 'Lorem ipsum dolor sit amet, consectetur adipiscing sed.'}}
     end
 
     assert_response :unprocessable_entity
+  end
+
+  test "should not create submission because email confirmation doesn't match" do
+    assert_no_difference('Submission.count') do
+      post contest_url, params: {submission: {first_name: 'Test', last_name: 'McTest', email: 'test@test.test', email_confirmation: 'test@test.com', slogan: 'This is a test!'}}
+    end
   end
 end
