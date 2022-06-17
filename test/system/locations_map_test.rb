bbf2c6947ec7ad814727b433eb12ccb90c67bf0c
Simon <simon@7lieues.io>
2022-06-17 12:23:34 +0200

    test: system test on contest page

--- test/system/locations_map_test.rb
+++ test/system/locations_map_test.rb
@@ -12,21 +12,21 @@ class SiteTest < ApplicationSystemTestCase
         # checks if there is at least one img tag whose src attribute starts with "https://live.staticflickr.com"
         assert_selector 'img[src^="https://live.staticflickr.com"]'
 
-        click_on 'img[src^="https://live.staticflickr.com"]'
+        first('img[src^="https://live.staticflickr.com"]').click
         # checks if modal has "display: block" inside "style" attribute
         assert_selector 'div[style*="display: block"]'
         # click on close cross
-        click_on '.btn-close'
+        first('.btn-close').click
         # checks if modal is now "display: none"
         assert_no_selector 'div.modal'
 
-        click_on 'img[src^="https://live.staticflickr.com"]'
+        first('img[src^="https://live.staticflickr.com"]').click
         # checks if modal has "show" class
         assert_selector 'div.modal.show'
         # clicks on button "Close"
         click_on "Close"
         # checks if modal is now "display: none"
-        assert_no_selector 'div.modal.show'
+        assert_no_selector 'div.modal'
     end
 
     test 'locations page displays riders markers' do
@@ -44,5 +44,21 @@ class SiteTest < ApplicationSystemTestCase
         page.assert_selector('.leaflet-popup-content-wrapper')
         first('.leaflet-popup-close-button').click
         page.assert_no_selector('.leaflet-popup-content-wrapper')
+    end
+
+    test 'form on contest page works' do
+        visit contest_url
+        # We fill the form with valid data and submit it
+        fill_in('First name', with: 'Test')
+        fill_in('Last name', with: 'McTest')
+        fill_in('Email', with: 'test.mctest@test.test')
+        fill_in('Email confirmation', with: 'test.mctest@test.test')
+        fill_in('Slogan', with: 'This is a test!')
+        click_button('Send')
+
+        # We check if the alert is sent back and we close it
+        assert_selector('.alert-success')
+        first('.btn-close').click
+        assert_no_selector('.alert .alert-success')
     end
 end
