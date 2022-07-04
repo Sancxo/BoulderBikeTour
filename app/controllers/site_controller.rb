6aec6dce3930beba8910ecb5a9ccf6f415adcf24
Simon <simon@7lieues.io>
2022-07-04 10:10:27 +0200

    refactor: better indentation

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -18,16 +18,16 @@ class SiteController < ApplicationController
     end
 
     def submit
-        @submission = Submission.new(submission_params)
-        respond_to do |format|
-            if @submission.save
-              format.html { redirect_to submissions_url, notice: "Submission was successfully sent." }
-            #   format.json { render :show, status: :created, location: @submission }
-            else
-              format.html { render :contest, status: :unprocessable_entity }
-              format.json { render json: @submission.errors, status: :unprocessable_entity }
-            end
-          end
+      @submission = Submission.new(submission_params)
+      respond_to do |format|
+        if @submission.save
+          format.html { redirect_to submissions_url, notice: "Submission was successfully sent." }
+        #   format.json { render :show, status: :created, location: @submission }
+        else
+          format.html { render :contest, status: :unprocessable_entity }
+          format.json { render json: @submission.errors, status: :unprocessable_entity }
+        end
+      end
     end
 
     def submission_params
